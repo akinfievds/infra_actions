7406398e431879be0298474342f98bcdb26f18d5
Akinfiev Dmitrii <akinfievds@gmail.com>
2021-09-14 18:25:59 +0300

    commit для теста

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Не так ли...')
 
 
 def second_page(request):
